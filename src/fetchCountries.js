b4a52a57c590fd8fd5e9ad147e69c3c20a3b9dbb
lisovenko02 <118771244+lisovenko02@users.noreply.github.com>
2023-05-05 05:14:07 +0300

    done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,6 @@
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+
 function fetchCountries(name) {
     const BASE_URL = 'https://restcountries.com/v3.1/name';
     return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
